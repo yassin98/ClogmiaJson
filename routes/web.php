b6a580a8a6f9162dbfebe11b8c2e14f64aea5eac
yassin98 <yassinassbaghi@gmail.com>
2018-09-14 10:45:22 +0200

    improved for mediawiki

--- routes/web.php
+++ routes/web.php
@@ -56,22 +56,23 @@ function featuresGlobal()
 	$features = array();
 	$CHROM_NAMES = namesGlobal();
 	foreach ($CHROM_NAMES as $key) {
-		//$data=make_feat($key);
-		//$features[$key] = $data;
+		$data=make_feat($key);
+		$features[$key] = $data;
 	}
 	return $features;
 }
 
-$router->get('/s', function (\Illuminate\Http\Request $request) use ($router) {
-
+$router->get('/', function (\Illuminate\Http\Request $request) use ($router) {
+	echo "Clogmia Json - Jbrowse";
 });
 
 
 $router->get('/features/{refseq}', function ($refseq, \Illuminate\Http\Request $request) use ($router) {
 
-   $category = "gene";
+$symbol = "_";
+	$seq = str_replace($symbol, "-5F", $refseq);
 
-	$ch = curl_init('http://clogmiawiki/wiki/Special:Ask/-5B-5BCategory:Gene-5D-5D-20-5B-5BLocation::'.$refseq.'-5D-5D/-3FStart/-3FEnd/-3FStrand/-3FName/-3FCategories/-3FId/mainlabel%3D/limit%3D100/prettyprint%3Dtrue/format%3Djson');
+	$ch = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/limit%3D500/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/-5B-5BItem:%2B-5D-5D-20-5B-5BPlain-20ref-20id::'.$seq.'-5D-5D/-3FStart/-3FEnd/-3FType/-3FPlain-20id/-3FStrand/-3FId/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $content = curl_exec($ch);
@@ -85,80 +86,87 @@ $router->get('/features/{refseq}', function ($refseq, \Illuminate\Http\Request $
 	$arrayIdx=array();
 	$exonsName=array();
 	$arrayExon=array();
+	$arraySubfeature=array();
+	$exonId=array();
 	for($i=0;$i<count($results);$i++)
 	{
 		$date = array_values($results)[$i];
 		$start = array_values($date)[0]["Start"][0];
 		$end = array_values($date)[0]["End"][0];
 		$strand = array_values($date)[0]["Strand"][0];
-		$name = array_values($date)[0]["Name"][0];
+		$name = array_values($date)[0]["Plain id"][0];
+		$type = array_values($date)[0]["Type"][0];
 		$id = array_values($date)[0]["Id"][0];
 		$vowels = "@Genome:clogmia6";
 		$id = str_replace($vowels, "", $id);
+		$id2 = str_replace($symbol, "-5F", $id);
 
-		$chj = curl_init('http://clogmiawiki/w/api.php?action=smwparent&format=json&retrieve=tree&title=Gene:'.$id.'@Genome:clogmia6&type=Contig,Exon');
+		$chj = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/limit%3D500/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/-5B-5BItem:%2B-5D-5D-20-5B-5BPlain-20ref-20id::'.$id2.'-5D-5D/-3FStart/-3FEnd/-3FType/-3FPlain-20id/-3FStrand/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
 		curl_setopt($chj, CURLOPT_RETURNTRANSFER, 1);
 	    $contentj = curl_exec($chj);
 	    curl_close($chj);
 	    $jsonj = json_decode($contentj, true);
-	   	$resultsj = $jsonj["smwparent"];
+	   	$resultsj = $jsonj["results"];
 		array_push($array3,$resultsj);
 
-		$dat = array('end' => $end, 'name' => $id, 'start' => $start, 'strand' => $strand, 'type' => $category, 'uniqueID' => $id);
-		array_push($res, $dat);
-
-		$vowel = "_";
-		$refseq = str_replace($vowel, " ", $refseq);
-		$idx = str_replace($vowel, " ", $id);
-		array_push($arrayId, $id);
-		array_push($arrayIdx, $idx);
+		$dat = array('end' => $end, 'name' => $id, 'start' => $start, 'strand' => $strand, /*'subfeatures' => array('NoInfo')*/ 'type' => $type, 'uniqueID' => $id);
+		array_push($res, $dat); //Result1
+		$vowels = "_";
+		$idd = str_replace($vowels, " ", $id);
+		array_push($arrayId, $idd);
 	}
 
 	for($j=0;$j<count($array3);$j++)
 	{
-		$datx = array_values($array3)[$j]["content"]["Contig:".$refseq."@Genome:clogmia6"]["link"]["Ref_id"]['Gene:'.$arrayId[$j].'@Genome:clogmia6']['link']['Ref_id']['Transcript:'.$arrayIdx[$j].'.t1@Genome:clogmia6'];
-		$startj = $datx['printouts']["Start"];
-		$endj = $datx['printouts']["End"];
-		$strandj = $datx['printouts']["Strand"];
-		$idj = $datx['printouts']["Id"];
-		$typex = $datx['type']["Categories"][0];
+		$date = array_values($array3)[$j]["Item:Annotation:".$arrayId[$j].".t1@Genome:clogmia6"]["printouts"];
+		$endj = $date["End"][0];
+		$startj = $date["Start"][0];
+		$strandj = $date["Strand"][0];
+		$idj = $date["Plain id"][0];
+		$typex = $date['Type'][0];
 		$vowels = "@Genome:clogmia6";
 		$idj = str_replace($vowels, "", $idj);
-		$exons = $datx['link']['Ref_id'];
-		array_push($exonsName, $exons);
-		$exonn=$exonsName[$j];
-		//var_dump($exonn);
-		$exonId=array_keys($exonn);
-		//print_r($exonId);
+		
+		$vowels = "_";
+		$idX = str_replace($vowels, "-5F", $idj);
+		array_push($exonId, $idX);
 		for($x=0;$x<count($exonId);$x++)
 		{
-			$exon2=$exonn[$exonId[$x]];
-			//print_r($exon2);
-			$startExon = $exon2['printouts']["Start"];
-			$endExon = $exon2['printouts']["End"];
-			$strandExon = $exon2['printouts']["Strand"];
-			$typeExon = $exon2['type']["Categories"][0];
-			$subfeatures2 = array(
- 			    'end' => $endExon,
-       			'start' => $startExon,
-       			'strand' =>$strandExon,
-      	  		'type' => $typeExon
-			);
-			array_push($arrayExon, $subfeatures2);
+			$chj = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/limit%3D500/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/-5B-5BItem:%2B-5D-5D-20-5B-5BPlain-20ref-20id::'.$exonId[$x].'-5D-5D/-3FStart/-3FEnd/-3FType/-3FPlain-20id/-3FStrand/-3FId/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
+			curl_setopt($chj, CURLOPT_RETURNTRANSFER, 1);
+		    $contentj = curl_exec($chj);
+		    curl_close($chj);
+		    $jsonj = json_decode($contentj, true);
+		   	$resultsSubfeature = $jsonj["results"];
+		   	$keySub=array_keys($resultsSubfeature);
+		   	for($y=0;$y<count($keySub);$y++)
+		   	{
+		   		$dateSub = array_values($resultsSubfeature)[$y]["printouts"];
+		   		$startExon = $dateSub["Start"][0];
+		   		$endExon = $dateSub["End"][0];
+				$strandExon = $dateSub["Strand"][0];
+				$typeExon = $dateSub['Type'][0];
+				$subfeatures2 = array(
+	 			    'end' => $endExon,
+	       			'start' => $startExon,
+	       			'strand' =>$strandExon,
+	      	  		'type' => $typeExon
+				);
+				array_push($arrayExon, $subfeatures2);
+		   	}
 		}
 		$subfeatures = array(array(
- 			    'end' => $endj,
-       			'name' => $idj,
-       			'start' => $startj,
-       			'strand' =>$strandj,
-       			'subfeatures' => $arrayExon,
-      	  		'type' => 'mRNA',
-        		'uniqueID' => $idj),
+		    'end' => $endj,
+			'name' => $idj,
+			'start' => $startj,
+			'strand' =>$strandj,
+			'subfeatures' => $arrayExon,
+	  		'type' => $typex,
+			'uniqueID' => $idj),
 		);
 		$data = $res[$j];
 		$data['subfeatures']=$subfeatures;
 		array_push($arrayPrueba, $data);
-		$arrayExon=array();
 	}
 
 	if ( $request->has('sequence') ) {
@@ -172,7 +180,6 @@ $router->get('/features/{refseq}', function ($refseq, \Illuminate\Http\Request $
 		$data = array('features' => $arrayPrueba);
 	}
 	return response()->json( $data);
-
 });
 
 
@@ -188,21 +195,23 @@ $router->get('/refSeqs.json', function (\Illuminate\Http\Request $request) use (
 	$seqs = seqsGlobal();
 	$resp = array();
 
-	$ch = curl_init('http://clogmiawiki/wiki/Special:Ask/-5B-5BCategory:Contig-5D-5D/-3FId/-3FSize/mainlabel%3D/limit%3D100/prettyprint%3Dtrue/format%3Djson');
+	$ch = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/limit%3D500/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/-5B-5BItem:%2B-5D-5D-20-5B-5BSize::%2B-5D-5D/-3FStart/-3FEnd/-3FSize/-3FId/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $content = curl_exec($ch);
     curl_close($ch);
     $json = json_decode($content, true);
-	$results = $json["results"];
+    $results = $json["results"];
 	$CHROM_NAMES = array();
 	for($i=0;$i<count($results);$i++)
 	{
 		$date = array_values($results)[$i];
 		$size = array_values($date)[0]["Size"][0];
+		$start = array_values($date)[0]["Start"][0];
+		$end = array_values($date)[0]["End"][0];
 		$key = array_values($date)[0]["Id"][0];
 		$vowels = "@Genome:clogmia6";
 		$key = str_replace($vowels, "", $key);
-		$data = array('length' => $size, 'name' => $key, 'start' => 0, 'end' => $size, 'seqChunkSize' => 20000);
+		$data = array('length' => $size, 'name' => $key, 'start' => $start, 'end' => $end, 'seqChunkSize' => 20000);
 		array_push($resp, $data);
 	}
 	return response()->json($resp);
@@ -233,8 +242,6 @@ $router->get('/names', function (\Illuminate\Http\Request $request) use ($router
 	return response()->json( $result );
 });
 
-
-//http://clogmiawiki/wiki/Special:Ask/-5B-5B:Contig:scaffold-20113@Genome:clogmia6-5D-5D/-3FSize/-3FRef-20id/mainlabel%3D/limit%3D50/prettyprint%3Dtrue/format%3Djson
 function refseqNames($refseq)
 {
 
@@ -248,34 +255,32 @@ function refseqNames($refseq)
 		$pos2 = strpos($refseq, $find2);
 		if ($pos2 === false) 
 		{
-			$ch = curl_init('http://clogmiawiki/wiki/Special:Ask/-5B-5B:Contig:'.$seq.'@Genome:clogmia6-5D-5D/-3FSize/-3FRef-20id/mainlabel%3D/limit%3D50/prettyprint%3Dtrue/format%3Djson');
+			$ch = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/limit%3D50/-5B-5B:Item:Annotation:'.$seq.'@Genome:clogmia6-5D-5D-20-5B-5BSize::%2B-5D-5D/-3FStart/-3FEnd/-3FRef-20id/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
 		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		    $content = curl_exec($ch);
 		    curl_close($ch);
 		    $json = json_decode($content, true);
 		    if (count($json)>0) {
 
-		    	$results = $json["results"]["Contig:".$seq2."@Genome:clogmia6"]["printouts"];
-		    	$end = $results["Size"][0];
-		    	$ref = $results["Ref id"][0]["fulltext"];
-		    	$location = array('ref' => $ref, 'start' => 0, 'end' => $end, 'tracks' => array("REST Test2 Track"), 'objectName' => $refseq);
-		    }
-		}
-	    else
-	    {
-			$ch = curl_init('clogmiawiki/wiki/Special:Ask/-5B-5B:Gene:'.$seq.'@Genome:clogmia6-5D-5D/-3FStart/-3FEnd/-3FRef-20id/mainlabel%3D/limit%3D50/prettyprint%3Dtrue/format%3Djson');
-		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		    $content = curl_exec($ch);
-		    curl_close($ch);
-		    $json = json_decode($content, true);
-		    if (count($json)>0) {
-
-		    	$results = $json["results"]["Gene:".$seq2."@Genome:clogmia6"]["printouts"];
+				$results = $json["results"]["Item:Annotation:".$seq2."@Genome:clogmia6"]["printouts"];
 		    	$start = $results["Start"][0];
 		    	$end = $results["End"][0];
 		    	$ref = $results["Ref id"][0]["fulltext"];
 		    	$location = array('ref' => $ref, 'start' => $start, 'end' => $end, 'tracks' => array("REST Test2 Track"), 'objectName' => $refseq);
 		    }
+		}
+	    else
+	    {
+			$ch = curl_init('http://wikimachine/wiki/Special:Ask/format%3Djson/link%3Dall/headers%3Dshow/searchlabel%3DJSON/class%3Dsortable-20wikitable-20smwtable/offset%3D/limit%3D50/-5B-5B:Item:Annotation:'.$seq.'@Genome:clogmia6-5D-5D/-3FStart/-3FEnd/-3FRef-20id/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue');
+		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		    $content = curl_exec($ch);
+		    curl_close($ch);
+		    $json = json_decode($content, true);
+			$results = $json["results"]["Item:Annotation:".$seq2."@Genome:clogmia6"]["printouts"];
+	    	$start = $results["Start"][0];
+	    	$end = $results["End"][0];
+	    	$ref = $results["Ref id"][0]["fulltext"];
+	    	$location = array('ref' => $ref, 'start' => $start, 'end' => $end, 'tracks' => array("REST Test2 Track"), 'objectName' => $refseq);
 		}
 		$data = array('name' => $refseq, 'location' => $location);
 		$result=array($data);
